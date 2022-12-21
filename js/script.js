75f860f73a40ee259c9ab9393930188105a8dc3f
Pham <phamduc.knn@gmail.com>
2022-12-21 08:16:16 +0700

    add file

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// function showHotelsList(){
+//     let boxList = document.getElementById("hotels-list");
+//     boxList.style.display = "block";
+//     boxList.style.top = "-180px"
+//     boxList.style.transitionDuration="all 2s"
+// }
